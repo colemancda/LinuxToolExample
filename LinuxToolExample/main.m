0cedd25f1a903e8005f30b418ac65730c6c4c408
Alsey Coleman Miller <alseycmiller@gmail.com>
2014-05-12 18:24:11 -0400

    first commit

---
+++ LinuxToolExample/main.m
@@ -0,0 +1,26 @@
+//
+//  main.m
+//  LinuxToolExample
+//
+//  Created by Alsey Coleman Miller on 5/12/14.
+//  Copyright (c) 2014 ColemanCDA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+        
+        
+        
+        
+    }
+    return 0;
+}
+
